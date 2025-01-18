71d4508f0869ffc007ae4635c8c98f7b12b81632
hasansid <hasansid158@gmail.com>
2025-01-19 04:18:03 +1100

    setup-chakra-3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ChakraProvider value={defaultSystem}>
     <App />
-  </StrictMode>,
+  </ChakraProvider>
 )
