0d9d1e3790248a6cf6e47408542c12ea80f29da5
hasansid <hasansid158@gmail.com>
2025-01-19 05:22:10 +1100

    added navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import App from './App.jsx';
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
-  <ChakraProvider value={defaultSystem}>
-    <App />
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>
 )
