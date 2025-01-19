747a200c4871f17285adffc539f53efb02d85fc5
hasansid <hasansid158@gmail.com>
2025-01-20 04:24:48 +1100

    added FE and configured SSR

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRouter from './routes/product.route.js';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/products', productRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+};
 
 app.listen(PORT, () => {
   connectDB();
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product"
+
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import { useEffect } from "react";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
+  const { products = [], fetchProducts } = useProductStore();
+
+  useEffect(() => {
+    if (products?.length) return;
+    fetchProducts();
+  }, [products, fetchProducts])
+
   return (
-    <div>HomePage</div>
+    <Container maxW='container.xl' py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        {!products.length && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+            No products found 😢&nbsp;
+            <Link to={"/create"}>
+              <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+
+    </Container>
   )
 }
 
